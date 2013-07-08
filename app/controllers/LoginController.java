9a2ef00c7bbd047cbbf1d0a1bc38bd44791f579c
Stanislav [spoilt_exile] Nepochatov <spoilt.exile@gmail.com>
2013-07-08 21:12:22 +0300

    Add login POST route and login error display;

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -6,9 +6,16 @@ import play.mvc.*;
 import views.html.*;
 
 public class LoginController extends Controller {
+	
+	private static String errorLogin = null;
   
     public static Result index() {
-        return ok(index.render("Вхід до системи..."));
+        return ok(login.render("Вхід до системи...", LoginController.errorLogin));
+    }
+    
+    public static Result login() {
+    	// TODO implement login method with cookies
+    	return redirect(routes.LoginController.index());
     }
   
 }
